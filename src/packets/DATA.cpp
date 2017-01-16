53143319475270a4ca21065e91354947653b34af
elad <eladsham@post.bgu.ac.il>
2017-01-16 20:30:57 +0200

    added packets

--- src/packets/DATA.cpp
+++ src/packets/DATA.cpp
@@ -6,16 +6,7 @@
 #include <vector>
 #include <packets/DATA.h>
 
-DATA::DATA(short packetSize, short block, std::vector<char> &data) : Packet((short)3)
-{
-    this->packetSize = packetSize;
-    this->block = block;
-    this->data = std::vector<char>(data.size());
-    for (int i = 0;i < data.size();i++)
-    {
-        this->data[i] = data[i];
-    }
-}
+DATA::DATA(short packetSize, short block, char* data) : Packet((short)3),packetSize(packetSize),block(block),data(data){}
 
 short DATA::getBlock()
 {
