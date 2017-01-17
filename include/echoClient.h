eb1b4cf4da3597116d05a7cdf25acac1d6669512
nir <abelnir90@gmail.com>
2017-01-17 17:37:51 +0200

    echoClient

--- include/echoClient.h
+++ include/echoClient.h
@@ -2,16 +2,18 @@
 #define CLIENT_CLIENT_H
 
 #include <packets/Packet.h>
-#include "ConnectionHandler.h"
+#include "connectionHandler.h"
 
 using std::string;
 
-class Client {
+class echoClient {
 private:
     static string trim(const string& str);
 public:
     static string checkFunction(string &line);
     static Packet* stringToPacket(string &line);
+    void SendOutput();
+    void ListenInput();
 };
 
 #endif //CLIENT_CLIENT_H
--- src/echoClient.cpp
+++ src/echoClient.cpp
@@ -10,6 +10,7 @@
 ConnectionHandler connection;
 bool running = true;
 
+
 int main(int argc, char *argv[]) {
     if (argc < 3) {
         std::cerr << "Usage: " << argv[0] << " host port" << std::endl << std::endl;
@@ -35,7 +36,7 @@ int main(int argc, char *argv[]) {
 
 
 
-void *SendOutput()
+void echoClient:: SendOutput()
 {
     while (running) {
         const short bufferSize = 1<<10;
@@ -54,10 +55,9 @@ void *SendOutput()
             break;
         }
     }
-    return NULL;
 }
 
-void *ListenInput()
+void echoClient:: ListenInput()
 {
     while (running) {
         std::string answer;
@@ -76,9 +76,9 @@ void *ListenInput()
 
     }
     connection.close();
-    return NULL;
-
 }
+
+
 
 
 
