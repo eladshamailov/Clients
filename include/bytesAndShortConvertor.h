5838cd543b471fd1ec6c8b835cca3eecea2ec631
elad <eladsham@post.bgu.ac.il>
2017-01-20 13:11:25 +0200

    Merge branch 'master' of https://github.com/eladshamailov/Clients
    # Conflicts:
    #	include/echoClient.h
    #	src/echoClient.cpp

---
+++ include/bytesAndShortConvertor.h
@@ -0,0 +1,16 @@
+//
+// Created by elad on 1/20/17.
+//
+
+#ifndef CLIENTS_BYTESANDSHORTCONVERTOR_H
+#define CLIENTS_BYTESANDSHORTCONVERTOR_H
+
+
+class bytesAndShortConvertor {
+public:
+    short bytesToShort(char* bytesArr){};
+    void shortToBytes(short num, char* bytesArr){};
+};
+
+
+#endif //CLIENTS_BYTESANDSHORTCONVERTOR_H
